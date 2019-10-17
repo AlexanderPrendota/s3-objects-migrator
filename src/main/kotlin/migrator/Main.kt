705702e171e7bc26290cca0387d8035de08a702f
Prendota <Prendota@mail.ru>
2019-10-17 10:53:20 +0300

    fix: correct getting object bytes

--- src/main/kotlin/migrator/Main.kt
+++ src/main/kotlin/migrator/Main.kt
@@ -1,7 +1,7 @@
 package migrator
 
 import software.amazon.awssdk.core.sync.RequestBody
-import software.amazon.awssdk.core.sync.ResponseTransformer
+import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.services.s3.model.GetObjectRequest
 import software.amazon.awssdk.services.s3.model.ListObjectsRequest
@@ -14,28 +14,34 @@ val SUFFIX_FROM = System.getenv("SUFFIX_FROM") ?: ""
 val SUFFIX_TO = System.getenv("SUFFIX_TO") ?: ""
 
 fun main() {
-    val s3Client = S3Client.create()
+    val s3Client = S3Client.builder().region(Region.EU_WEST_1).build()
     val summaries = s3Client
         .listObjects(ListObjectsRequest.builder().bucket(BUCKET_FROM).build())
         .contents()
     val startTime = System.currentTimeMillis()
     summaries
-        .map { it.key() + SUFFIX_FROM }
+        .map { it.key() }
+        .subList(0, 10)
         .parallelStream()
         .forEach { key ->
             val getRequest = GetObjectRequest.builder()
                 .bucket(BUCKET_FROM)
                 .key(key)
                 .build()
-            val responseInputStream = s3Client.getObject(getRequest, ResponseTransformer.toInputStream())
-            val s3Object = responseInputStream.response()
-            println("Sending data for key = ${key}...")
+            val bytesResponse = s3Client.getObjectAsBytes(getRequest)
+            println("Sending data for key = ${key}... . Time since start: ${System.currentTimeMillis() - startTime}ms")
             val putRequest = PutObjectRequest.builder()
                 .bucket(BUCKET_TO)
-                .metadata(s3Object.metadata())
-                .key(key + SUFFIX_TO)
+                .metadata(bytesResponse.response().metadata())
+                .key(key)
                 .build()
-            s3Client.putObject(putRequest, RequestBody.fromInputStream(responseInputStream, s3Object.contentLength()))
+            s3Client.putObject(
+                putRequest,
+                RequestBody.fromInputStream(
+                    bytesResponse.asByteArray().inputStream(),
+                    bytesResponse.asByteArray().size.toLong()
+                )
+            )
         }
 
     val endTime = System.currentTimeMillis()
